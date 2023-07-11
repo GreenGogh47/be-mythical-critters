4dafde814c1cdcea767b44eef0291c75a2b1d2b4
Chris Crane <chrisjcrane@gmail.com>
2023-07-11 11:43:52 -0600

    complete centaur

--- exercises/centaur.js
+++ exercises/centaur.js
@@ -4,30 +4,71 @@ class Centaur {
     this.breed = breed;
     this.cranky = false;
     this.standing = true;
-    this.activity = 0
+    this.layingDown = !this.standing;
+    this.activity = 0;
   }
 
   shoot() {
-    this.activity += 1;
-    if (this.activity < 4) {
-      return "Twang!!!"
+    this.activity++;
+    // this is the same as this.activity += 1
+    // you can do the same with this.activity--
+    if (this.activity < 3 && this.standing) {
+      return "Twang!!!";
     } else {
-      this.no()
+      return this.no();
     }
   }
 
   run() {
-    this.activity += 1;
-    if (this.activity < 4) {
-      return "Clop clop clop clop!!!"
+    this.activity++;
+    if (this.activity < 3 && this.standing) {
+      return "Clop clop clop clop!!!";
     } else {
-      this.no()
+      return this.no();
+    }
+  }
+
+  layDown() {
+    this.layingDown = true;
+    this.standing = false;
+  }
+  
+  standUp() {
+    this.layingDown = false;
+    this.standing = true;
+  }
+
+  // toggleStand() {
+  //   this.layingDown = !this.layingDown
+  //   this.standing = !this.standUp
+  // }
+
+  sleep() {
+    if (this.standing) {
+      return "NO!";
+    } else {
+      this.activity = 0;
+      this.cranky = false;
+      return "ZZZZ";
+    }
+  }
+
+  drinkPotion() {
+    if (this.standing) {
+      if (!this.cranky) {
+        this.cranky = true;
+      } else {
+        this.activity = 0;
+        this.cranky = false;
+      }
+    } else {
+      return 'Not while I\'m laying down!'
     }
   }
 
   no() {
-    this.cranky = true
-    return "NO!"
+    this.cranky = true;
+    return "NO!";
   }
 }
 
